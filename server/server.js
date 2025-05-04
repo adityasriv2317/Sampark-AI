11e843d3a5069e18164541e0b955ebc88d12ef6a
adityasriv2317 <adityaxia9237@gmail.com>
2025-05-05 01:58:56 +0530

    a

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const { sendMail } = require("./mailController");
 const { scheduleBulkEmails } = require("./scheduler");
 const app = express();
 require("dotenv").config();
 let port = 3000;
+
+app.use(cors());
 
 app.use(express.json());
 
