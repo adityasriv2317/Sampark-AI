58003773d44c8d364edea7483b2e872a93921670
adityasriv2317 <adityaxia9237@gmail.com>
2025-05-05 02:41:51 +0530

    ui

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -46,7 +46,6 @@ const Navbar = () => {
         {/* Mobile Menu Toggle Button using Lucide icons */}
         <button className="md:hidden text-2xl" onClick={toggleMenu}>
           {isOpen ? <X size={28} /> : <Menu size={28} />}{" "}
-          {/* Use Lucide Menu/X icons */}
         </button>
       </div>
 
@@ -64,7 +63,6 @@ const Navbar = () => {
             text-center md:text-left /* Center text on mobile */
           `}
         >
-          {/* Nav Items - Adjusted hover effect and added icons */}
           <li className="my-2 md:my-0">
             <Link
               to="/"
@@ -85,41 +83,7 @@ const Navbar = () => {
               New Mailing
             </Link>
           </li>
-          <li className="my-2 md:my-0">
-            <Link
-              to="/my-mails"
-              className="flex items-center justify-center md:justify-start gap-2 py-2 px-3 md:p-0 rounded hover:bg-indigo-700 md:hover:bg-transparent md:hover:text-indigo-200 transition-colors duration-200"
-              onClick={handleLinkClick}
-            >
-              <Mails size={18} /> {/* My Mails Icon */}
-              My Mails
-            </Link>
-          </li>
         </ul>
-      </div>
-      {/* Login/Signup Buttons */}
-      {/* Hidden on mobile (handled by menu toggle), flex on medium+ */}
-      <div
-        className={`hidden md:flex items-center gap-4 ${
-          isOpen ? " flex flex-col items-center w-full mt-4" : ""
-        }`}
-      >
-        <Link
-          to="/login"
-          className="flex items-center gap-1 bg-white text-indigo-600 px-4 py-2 rounded font-medium hover:bg-indigo-100 transition-colors duration-200 w-full md:w-auto justify-center"
-          onClick={handleLinkClick}
-        >
-          <LogIn size={16} />
-          Login
-        </Link>
-        <Link
-          to="/signup"
-          className="flex items-center gap-1 bg-indigo-700 text-white px-4 py-2 rounded font-medium hover:bg-indigo-800 transition-colors duration-200 w-full md:w-auto justify-center"
-          onClick={handleLinkClick}
-        >
-          <UserPlus size={16} />
-          Sign Up
-        </Link>
       </div>
     </nav>
   );
